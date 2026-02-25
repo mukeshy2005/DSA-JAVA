ef4b4d43bf21265cc912a6a34dde5a40282f917d
Mukesh Yadav <my653197@gmail.com>
2026-02-25 20:56:01 +0530

    added example for bitwise operator

--- javaFullCourse/Demo2.java
+++ javaFullCourse/Demo2.java
@@ -17,5 +17,15 @@ public class Demo2 {
         System.out.println(f);// true
         boolean g = (a<=b);// any one of the condition is true then it will return true
         System.out.println(g);// true
+        byte k = 1;
+        k = (byte) (k<<7);
+        // k = (byte) (k<<1);// this will give 0 as output we need to understand the resason behind it 
+        int i = k<<1;
+        System.out.println(i); //outptt will be -128 because when we left shift the bits of 1 by 7 positions, it becomes 10000000 in binary, which is -128 in decimal.
+         int m = 1;
+        m =  (m<<31);// this will give 0 as output because when we left shift the bits of 1 by 31 positions, it becomes 10000000000000000000000000000000 in binary, which is -2147483648 in decimal. However, since m is an int, it can only hold values up to 2147483647, so it wraps around and becomes 0.
+        // m = (m<<1);
+        System.out.println(m);// output will be 128 because when we left shift the bits of 1 by 7 positions, it becomes 10000000 in binary, which is 128 in decimal.
+
     }
 }
