397b67bc2d685d8297599f779b24b3d25e9ff2ad
Mukesh Yadav <my653197@gmail.com>
2026-02-26 21:37:50 +0530

    created a new folder and Demo.java file

--- javaFullCourse/Lecture07/Solution.java
+++ javaFullCourse/Lecture07/Solution.java
@@ -148,6 +148,14 @@ public class Solution {
 				break;
 		}
 				*/
+		//Question 10: Simple Calculator (switch)
+
+		System.out.println("Enter first number");
+		int num1 = sc.nextInt();
+		System.out.println("Enter second number");
+		int num2 = sc.nextInt();
+
+		switch()
 
 
 
---
+++ javaFullCourse/Lecture08/Demo.java
@@ -0,0 +1,17 @@
+package javaFullCourse.Lecture08;
+
+public class Demo {
+    public static void main(String [] args) {
+	    byte i = 1;
+
+	    // while ( i <= 10 ) {
+		//     System.out.println(i);
+		//     i++;
+	    // }
+
+        int j = 0;
+        while ( j++ < 10 ) {
+            System.out.println(j);
+        }
+    }
+}
