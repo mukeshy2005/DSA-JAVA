ba7aefa8333fd8e5e6772fe98c7cb43a64c64875
Mukesh Yadav <my653197@gmail.com>
2026-02-26 20:59:55 +0530

    solution till question 9

---
+++ javaFullCourse/Lecture07/Solution.java
@@ -0,0 +1,156 @@
+
+import java.util.*;
+public class Solution {
+	public static void main(String [] args) {
+		//Question 1: Positive Number Check
+		Scanner sc = new Scanner(System.in);
+		//System.out.println("enter a integer number:");
+		//int num = sc.nextInt();
+		
+	/*	if(num > 0 ) {
+			System.out.println("number is positive");
+		}
+		else{
+			System.out.println("number is negative");
+		}
+		*/
+		//Question 2: Even or Odd
+		/*
+		if(num % 2 == 0) {
+			System.out.println("number is even");
+		}
+		else{
+			System.out.println("numer is odd");
+               }*/
+/*
+		//Question 3: Voting Eligibility
+		System.out.println("enter your age");
+                int age = sc.nextInt();
+
+		if( age >= 18 ) {
+			System.out.println("eligible to vote");
+		}
+		else {
+			System.out.println("not eligible");
+		}*/
+
+	/*	//Question 4: Greater of Two Numbers
+		System.out.println("enter number1:");
+		int num1 = sc.nextInt();
+		System.out.println("enter number2:");
+		int num2 = sc.nextInt();
+
+		if( num1 > num2 ) {
+			System.out.println(num1+" is greater then "+num2);
+		}else {
+			System.out.println(num2 +" is greater then "+num1);
+		}
+		*/
+/*
+		//Question 5: Temperature Category
+		System.out.println("enter temperature");
+		int temp = sc.nextInt();
+
+		if( temp < 10 ) {
+			System.out.println("Cold");
+		}
+		else if ( temp >= 10 && temp < 25 ) {
+			System.out.println("Moderate");
+		}
+		else {
+			System.out.println("Hot");
+		}
+*/
+        /*        
+		//Question 6: Student Grade Calculator
+		System.out.println("Enter your marks between 0-100");
+		int marks = sc.nextInt();
+
+		if ( marks >= 90 ) {
+			System.out.println("Grade A");
+		}
+		else if ( marks >= 75 ) {
+			System.out.println("Grade B");
+		}
+		else if ( marks >= 65 ) {
+			System.out.println("Grade C");
+		}
+		else if ( marks >=40 ) {
+			System.out.println("Grade D");
+		}
+		else {
+			System.out.println("you are fail");
+		}
+
+		*/
+		/* 
+		//Question 7: Largest of Three Numbers
+		System.out.println("Enter number1:");
+		int num1 = sc.nextInt();
+		System.out.println("Enter number2:");
+		int num2 = sc.nextInt();
+		System.out.println("Enter number3:");
+		int num3 = sc.nextInt();
+
+		if (num1 >= num2 && num1 >= num3) {
+            System.out.println("Largest number is " + num1);
+        } 
+        else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("Largest number is " + num2);
+        } 
+        else {
+            System.out.println("Largest number is " + num3);
+        }
+			*/
+
+			/* 
+		//Question 8: Number Sign Check
+	System.out.println("Enter number");
+        int number = sc.nextInt();
+	
+	if ( number > 0 ) {
+		System.out.println("number is positive");
+	}
+	else if (number < 0 ) {
+		System.out.println("number is negative");
+	}
+	else {
+		System.out.println("number is zero ");
+	}
+		*/
+
+		/* 
+		// Question 9: Day of Week (switch)
+		System.out.println("Enter a number between 1-7");
+		int number = sc.nextInt();
+		// we will switch now 
+		switch(number) {
+			case 1: 
+				System.out.println("Monday");
+				break;
+			case 2:
+				System.out.println("Tuesday");
+				break;
+			case 3:
+				System.out.println("Wednesday");
+				break;
+			case 4:
+				System.out.println("Thursday");
+				break;
+			case 5:
+				System.out.println("Friday");
+				break;
+			case 6:
+				System.out.println("Saturday");
+				break;
+			case 7:
+				System.out.println("Sunday");
+				break;
+		}
+				*/
+
+
+
+}
+
+}
