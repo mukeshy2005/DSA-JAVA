60a42215441f2ee5fac73bba9bfc2714642c8453
Mukesh Yadav <my653197@gmail.com>
2026-02-26 11:09:38 +0530

    if else concept

---
+++ javaFullCourse/Lecture07/Demo01.java
@@ -0,0 +1,35 @@
+public class Demo01 {
+    public static void main(String [] args) {
+       /* boolean b = true ;
+        // Selection statement 
+        //Normal if
+        if(b == true) {
+             System.out.println(" b is true");
+        }
+        else {
+             System.out.println(" b is not true");
+        }
+
+        int i = 10;
+	if(i%2 == 0) {
+          System.out.println("i is even number ");
+    }
+    else {
+	    System.out.println(" i is odd number ");
+    } */
+
+	    int i = 10;
+	    if(i < 20 ) {
+		    if(i > 5 ) {
+			    System.out.println("i is true ");
+		    }
+		    else {
+			    System.out.println("i is not true ");
+		    }
+	    }
+	    else{
+		    System.out.println("will not execute ");
+	    }
+
+    }
+ }
