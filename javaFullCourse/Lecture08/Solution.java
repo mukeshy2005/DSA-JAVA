12b43d1274318112b97f13218f9bb629dcdbd3ac
Mukesh Yadav <my653197@gmail.com>
2026-03-01 12:31:38 +0530

    question 1 and 2 solved

---
+++ javaFullCourse/Lecture08/Solution.java
@@ -0,0 +1,35 @@
+package javaFullCourse.Lecture08;
+import java.util.*;
+public class Solution {
+   // contain Solution of Lecture 08
+   public static void main(String [] args) {
+	   Scanner sc = new Scanner(System.in);
+       /* 
+       // Question 1: Print a Square Pattern
+	   System.out.println("Enter number of rows ");
+	   int n = sc.nextInt();
+	   
+	   for ( int i = 1 ; i <= n ; i++ ) {
+		   for ( int j = 1 ; j <= n ; j++ ){
+			   System.out.print("*");
+		   }
+		   System.out.println();
+	   }
+           */
+
+       //Question 2: Print Perpendicular Line (Easy)
+       System.out.println("enter number ");
+       int n = sc.nextInt();
+       for(int i = 1; i <= n ; i++) {
+           for(int j = 1; j <= n; j++) {
+		   if( i == 1 || j == 3 ) {
+			   System.out.print("*");
+   }else {
+	   System.out.print(" ");
+   }
+	   }
+   System.out.println();
+       }
+   }
+
+}
