e552c89ad3cd163aad03c1aaa284a02ef7124085
Mukesh Yadav <my653197@gmail.com>
2026-03-01 14:04:15 +0530

    question 7

--- javaFullCourse/Lecture08/Solution.java
+++ javaFullCourse/Lecture08/Solution.java
@@ -66,7 +66,18 @@ public class Solution {
 //             }
 //             System.out.println();
 //         }
-//    }
+// Question 7: Print Continuous Number Triangle (Medium)
+      int number = 1;
+
+for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+        System.out.print(number + " ");
+        number++;
+    }
+    System.out.println();
+}
+     
+   }
   
 
 }
