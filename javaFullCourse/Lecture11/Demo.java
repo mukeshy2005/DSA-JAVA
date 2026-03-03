17ed07cc55b8a43a83ad5cb9a496c7f5eb43f032
Mukesh Yadav <my653197@gmail.com>
2026-03-03 19:19:30 +0530

    Lecture 11 and java functions

---
+++ javaFullCourse/Lecture11/Demo.java
@@ -0,0 +1,16 @@
+package javaFullCourse.Lecture11;
+
+public class Demo {
+    public static void main(String [] args) {
+        // Functioon overloading in java
+        // starting with the basic example 
+         int i;
+        int j;
+        System.out.println(sum(5,6));
+    }
+    static int sum(int a, int b){
+        int result = a+b;
+        return result;
+    }
+    
+}
