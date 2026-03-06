15285357543b581331b96b27ff6126066da3166f
Mukesh Yadav <my653197@gmail.com>
2026-03-06 11:02:41 +0530

    Lecture 13 constructor

---
+++ javaFullCourse/Lecture13/Demo1.java
@@ -0,0 +1,35 @@
+package javaFullCourse.Lecture13;
+
+public class Demo1 {
+    public static void main(String [] args) {
+         Student s1 = new  Student();
+        //  s1.name = "mukesh";
+        //  s1.age = 20;
+        //  s1.rollNum = 42;
+        //  s1.college = "DSRU";
+        // if value are not given to instance variable that they store default vaues 
+        // tht is 
+        /* int = 0
+        boolean = false
+        String = null
+        float = 0.0*/
+         
+         System.out.println(s1.name);
+         System.out.println(s1.rollNum);
+         
+         
+        
+    }
+     }
+   static class Student{
+       String name;
+       int age ;
+       int rollNum;
+       String college;
+       
+       void markAttendance(){
+           System.out.println(name+" is present");
+       }
+   
+   
+}
