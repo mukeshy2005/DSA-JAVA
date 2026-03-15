71432c38ead8acd59f0fa687defa8f45616a0b94
Mukesh Yadav <my653197@gmail.com>
2026-03-15 13:45:58 +0530

    inheritance (simple inheritance )

---
+++ javaFullCourse/Lecture16/Demo1.java
@@ -0,0 +1,30 @@
+package javaFullCourse.Lecture16;
+// inheritance
+public class Demo1 {
+    public static void main(String[] args){
+        EngineeringStudent es = new  EngineeringStudent();
+        es.markAttendance();
+        es.attendLab();
+
+        //making another object to test can we call Engineering student method from student
+        Student sc = new Student();
+        sc.markAttendance();
+//        sc.attendlab(); //showing error
+    }
+}
+// Student is parent class (super class )
+class Student {
+    String name;
+    int rollNum;
+
+    void markAttendance(){
+        System.out.println("Marking attendance");
+    }
+}
+
+// child class
+class EngineeringStudent extends Student{
+    void attendLab(){
+        System.out.println("Attending Lab");
+    }
+}
