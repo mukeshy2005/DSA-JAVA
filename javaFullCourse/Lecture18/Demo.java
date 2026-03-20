ffe2ad42b8d27400e777c39bc83e667af167985e
Mukesh Yadav <my653197@gmail.com>
2026-03-20 21:03:43 +0530

    Lecture 18 public class

---
+++ javaFullCourse/Lecture18/Demo.java
@@ -0,0 +1,12 @@
+package javaFullCourse.Lecture18;
+
+public class Demo {
+    // we will discuss 
+    // two main question
+    //1. why only one public class per folder 
+    //2.why name of the class is same as the name of the file
+}
+
+// public class Demo{
+//     // Demo is aleady  defined  we cannot create another public class in  same file
+// }
