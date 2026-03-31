c8cf1000deb6c64eee44911abe9e0a44db22bfc8
Mukesh Yadav <my653197@gmail.com>
2026-03-31 17:21:38 +0530

    key properties with png is uploaded

--- javaFullCourse/Lecture19/Demo3.java
+++ javaFullCourse/Lecture19/Demo3.java
@@ -3,18 +3,23 @@ package javaFullCourse.Lecture19;
 public class Demo3 {
     public static void main(String[] args) {
 
+        Outer outer = new Outer();
+    Outer.Inner inner = new Outer.Inner(outer);
 
-    Outer.Inner inner = new Outer.Inner();
-    Outer outer = new Outer();
-        inner.printNum(outer);
+        inner.printNum();
     }
 }
 class Outer{
     static int x = 10;
     int y = 3;
     static class Inner{
+        //creating a variable
+        Outer outer;
+        Inner(Outer outer){
+            this.outer = outer;
+        }
 
-        static void printNum(Outer outer){
+        void printNum(){
             System.out.println(x);
             System.out.println(outer.y);
             // there are two was to acces non static variale one though passsing reference
@@ -22,5 +27,16 @@ class Outer{
 
         }
 
+    }
+}
+
+class BankAccount{
+    private static class interestCalculator{
+        static double yearlyInterest(double principal, double rate ){
+            return principal * rate;
+        }
+    }
+    public  double computeInterest(double principal){
+        return interestCalculator.yearlyInterest(principal, 2);
     }
 }
