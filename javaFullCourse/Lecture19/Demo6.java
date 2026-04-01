56088a220f8f71462116a43f4e619fe7c795187f
Mukesh Yadav <my653197@gmail.com>
2026-04-01 13:45:47 +0530

    effective final variable

--- javaFullCourse/Lecture19/Demo6.java
+++ javaFullCourse/Lecture19/Demo6.java
@@ -7,10 +7,18 @@ public class Demo6 {
     }
 }
 class Outer4{
+    private int x = 19; // instance variable
+    static int y = 10;
     void fun(){
+        // making a local variable
+        int a = 13;
+        a++; // we cannot modify it it is effectively final
         class Local{
             void sayHello(){
                 System.out.println("hello");
+                System.out.println(x);
+                System.out.println(y);
+                System.out.println(a); //error of effective final variable
             }
         }
         Local local = new Local();
