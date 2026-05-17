e3a2d5f2eb51a6a815f67edf9d1e6a2352f01a33
Mukesh Yadav <my653197@gmail.com>
2026-05-17 20:52:38 +0530

    object method study including Nullpointer exception and class cast exception

--- javaFullCourse/Lecture22/Demo1.java
+++ javaFullCourse/Lecture22/Demo1.java
@@ -4,7 +4,9 @@ public class Demo1 {
     public static void main(String [] args){
         Student1 s1 = new Student1("mukesh",21);
         Student1 s2 = new Student1("mukesh",21);
-        System.out.println(s1.equals(s2)); // this will return false (will only compare the references
+        Student1 s3 = null; // this  way we will recieve null  pointer exception
+        Integer i = 28; // class cast exception
+        System.out.println(s1.equals(i)); // this will return false (will only compare the references
     }
 }
 // we can Override the equals method
@@ -17,6 +19,15 @@ class Student1 {
     }
     @Override
     public boolean equals(Object obj){
+        // to remove null pointer exception
+        if (obj == null){
+            return false;
+        }
+        // check if both classes are of student type
+        // if not check will receive class cast exception
+        if (obj.getClass()!= this.getClass()){
+            return false;
+        }
         Student1 s = (Student1)obj;
         return (this.name == s.name && this.age == s.age);  // will return because comparing the value instead of reference variable
         // use the reference variable used above
