ab8cb1c7dae90a8eaad979f24a2e4edcfc0ed42c
Mukesh Yadav <my653197@gmail.com>
2026-06-24 15:07:57 +0530

    IOC implementation and @Primary at one bean

---
+++ FoodService/src/main/java/in/webinex/AppConfig.java
@@ -0,0 +1,10 @@
+package in.webinex;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("in.webinex")
+public class AppConfig {
+
+}
